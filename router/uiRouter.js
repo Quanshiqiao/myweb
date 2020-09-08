7cc5de058637c9d244402f115d370d6c17e6ca6d
QUANSHIQIAO <email@1756475096@qq.com>
2020-09-08 16:33:06 +0800

    five

--- router/uiRouter.js
+++ router/uiRouter.js
@@ -15,7 +15,7 @@ router.get('/',(request,respon)=>{
 //游戏
 router.get('/game',(req,res)=>{
     let filePaht=resolve(__dirname,'../public/game.html');
-    respon.sendFile(filePath)
+    res.sendFile(filePath)
 });
 
 module.exports = router;
